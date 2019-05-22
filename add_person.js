a20fc1c3ec0e971fa4d6587126101f8bb3161be2
Dan <dan.zajac@mac.com>
2019-05-22 15:32:31 +0000

    Add person w/  knex

--- add_person.js
+++ add_person.js
@@ -1,4 +1,5 @@
 const settings = require("./settings"); // settings.json
+const pg = require("pg");
 
 var knex = require('knex')({
   client: 'pg',
@@ -12,11 +13,18 @@ var knex = require('knex')({
   
   }
 });
-firstname = process.argv[2];
-knex.select().from('famous_people').where('first_name', firstname).asCallback(function(err, result) {
-  if (err) {
-    return console.log("Connection Error", err);
-  }
-  console.log(result);
-})
 
+const query = function(new_person){ 
+  knex('famous_people').insert(new_person)
+  .finally(function() { knex.destroy(); });
+}
+
+const newPerson =
+  {
+    first_name : process.argv[2],
+    last_name : process.argv[3],
+    birthdate : process.argv[4]
+  }
+
+
+console.log(query(newPerson));
